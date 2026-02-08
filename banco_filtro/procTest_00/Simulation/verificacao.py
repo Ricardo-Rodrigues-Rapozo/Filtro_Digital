c780d229271ed356b5f8188a2e2d25f9538f4676
Ricardo Rapozo <ricardo.rapozo@estudante.ufjf.br>
2026-02-08 14:25:23 -0300

    feat: Código com saídas do sapho com problema

--- banco_filtro/procTest_00/Simulation/verificacao.py
+++ banco_filtro/procTest_00/Simulation/verificacao.py
@@ -14,7 +14,8 @@ f0 = 60
 Nppc = 256
 Fs = f0 * Nppc
 Ts = 1/Fs
-Ganho_saida_sapho = 1000000    ## ganho usado inicialmente 1000000
+Ganho_saida_sapho = 1    ## ganho usado inicialmente 1000000
+
 #Nc = 60
 #t = np.arange(Nc * Nppc) * Ts
 #x = np.cos(2*np.pi* f0 * t) + 0.5*np.cos(2*np.pi* 3*f0 * t) + 0.5*np.cos(2*np.pi*5*f0 * t)
@@ -28,12 +29,14 @@ Ganho_saida_sapho = 1000000    ## ganho usado inicialmente 1000000
 x = np.loadtxt('55HZ.txt')
 Nc = len(x) // Nppc
 t = np.arange(len(x)) * Ts
-zc = np.where(np.diff(np.sign(x)))[0]
-Ns_cycle = np.mean(np.diff(zc))*2
-print(Ns_cycle)
 
-x / np.max(np.abs(x))
+# zc = np.where(np.diff(np.sign(x)))[0]
+# Ns_cycle = np.mean(np.diff(zc))*2
+# print(Ns_cycle)
+
+#x = x / np.max(np.abs(x))
 np.savetxt('input_0.txt', x*(2**14), fmt='%d')
+x = x * (2**14) 
 
 print(Nc)
 print(f"Tamanho de x: {len(x)}")
